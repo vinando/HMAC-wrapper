373868e54bcb2f4db7950f1ca467b45f7567b26b
Abdol Gofo <abdolgofo@Abdols-MacBook-Pro.local>
2020-04-14 04:17:01 +0700

    add header when call request

--- wrapper.go
+++ wrapper.go
@@ -29,33 +29,42 @@ func Init(clientID string, clientSecret string, baseURL string) *HMACWrapper {
 var client = &http.Client{}
 
 // param endpoint must start with a '/'
-func (wp *HMACWrapper) DoGet(endpoint string) (resp interface{}, err error) {
+func (wp *HMACWrapper) DoGet(endpoint string, headers map[string]string) (resp interface{}, err error) {
 	signature, t := wp.constructSignature("GET " + endpoint + " HTTP/1.1")
 	req, err := http.NewRequest("GET", wp.BaseURL+endpoint, nil)
 	if err != nil {
-		log.Fatalf("Error initiating request")
+		fmt.Println("Error initiating request")
+		return nil, err
+	}
+	for key, val := range headers {
+		req.Header.Set(key, val)
 	}
 	req.Header.Set("Date", t)
 	req.Header.Set("Authorization", fmt.Sprintf("hmac username=\"%s\", algorithm=\"hmac-sha256\", headers=\"date request-line\", signature=\"%s\"", wp.ClientID, signature))
 	res, err := client.Do(req)
 	if err != nil {
-		log.Fatalf("Error sending request")
+		fmt.Println("Error sending request")
+		return nil, err
 	}
 	defer res.Body.Close()
 
 	err = json.NewDecoder(res.Body).Decode(&resp)
 	if err != nil {
-		log.Fatalf("Error decode json body")
+		fmt.Println("Error decode json body")
+		return nil, err
 	}
 	return resp, nil
 }
 
-func (wp *HMACWrapper) DoPost(endpoint string, body []byte) (resp interface{}, err error) {
+func (wp *HMACWrapper) DoPost(endpoint string, body []byte, headers map[string]string) (resp interface{}, err error) {
 	signature, t := wp.constructSignature("POST " + endpoint + " HTTP/1.1")
 	digest, err := wp.constructDigest(body)
 	req, err := http.NewRequest("POST", wp.BaseURL+endpoint, bytes.NewBuffer(body))
 	if err != nil {
 		log.Fatalf("Error initiating request")
+	}
+	for key, val := range headers {
+		req.Header.Set(key, val)
 	}
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("Date", t)
